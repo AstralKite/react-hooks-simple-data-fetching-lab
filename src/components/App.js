7c40913570c436179b94bd3de24c94afee8cc872
Fausto Ramirez <ramirezmedia@gmail.com>
2023-03-04 12:20:00 -0500

    commit tree 2ecd26dddaf0de86f7d3bba10212f3ca67610a1e

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App(){
+
+    //bool update
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    //image update
+    const [dogImage, setDogImage] = useState(null);
+
+
+    useEffect( ()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then( (r)=> r.json())
+            .then( (data)=> {
+                setDogImage(data.message);
+                setIsLoaded(true);
+            })
+    }, []);
+
+    if(!isLoaded) return <p>Loading...</p>;
+
+    return <img src={dogImage} alt="A Random Dog"/>;
+}
+
+
+
+export default App;
